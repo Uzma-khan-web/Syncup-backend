c06766cbfcf10c2443943b1b236e8067dc8809a0
Uzma Khan <https://github.com/uzma-khan-web>
2026-04-03 02:21:47 +0530

    Fix: Synced settings.py with Railway MySQL variables

--- core/settings.py
+++ core/settings.py
@@ -74,11 +74,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME', default='syncup_db'),
-        'USER': env('DB_USER', default='root'),
-        'PASSWORD': env('DB_PASSWORD', default='Uzma@1609'),
-        'HOST': env('DB_HOST', default='127.0.0.1'),
-        'PORT': env('DB_PORT', default='3306'),
+        'NAME': env('MYSQLDATABASE', default='railway'),
+        'USER': env('MYSQLUSER', default='root'),
+        'PASSWORD': env('MYSQLPASSWORD', default=''),
+        'HOST': env('MYSQLHOST', default='mysql.railway.internal'),
+        'PORT': env('MYSQLPORT', default='3306'),
     }
 }
 
@@ -131,6 +131,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # WhiteNoise optimization for storage
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
